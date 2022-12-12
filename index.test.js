fe4a995bc0b0abdc7dd00f384e0b687aeaafb913
Eric Drake <puturosule@gmail.com>
2022-12-12 14:01:38 -0700

    added unit tests

--- index.test.js
+++ index.test.js
@@ -15,3 +15,57 @@ let testData = {
     floor: 5,
     roomNumber: 3
   }
+
+// test('test function', () => {
+//   expect(2).toBe(2);
+// });
+
+// test('first name should be patten', () => {
+//   expect(testData.firstName).toBe('Patten');
+// });
+
+//groups formatTitle tests
+describe('formatTitle tests', () => {
+  test('formatTitle should return a string', () => {
+    expect(typeof formatTitle(testData.title)).toBe('string');
+  });
+
+  test('formatTitle should return a string with each word capitalized', () => {
+    let formmattedTitle = formatTitle(testData.title);
+    expect(formmattedTitle).toBe('Nulla Ac');
+  });
+});
+
+describe('shortedBio tests', () => {
+  let shortBio = shortenBio(testData.bio);
+  test('shortenedBio should return a shorter string', () => {
+    expect(shortBio.length < testData.bio.length).toBeTruthy();
+  });
+
+  test('shortenBio should add ... to the end', () => {
+    expect(shortBio).toMatch(/\.\.\.$/);
+    //expect(shortenBio(testData.bio)).toContain('...');
+  });
+});
+
+describe('convertLength tests', () => {
+  let result = convertLength(testData.length);
+
+  test('convertLength should return an array with two elements', () => expect(result).toHaveLength(2));
+
+  test('convertLength should return a minute value that is less than 60', () => expect(result[1]).toBeLessThan(60));
+});
+
+test('toStrictEqual', () => {
+  const myObj = {myNum: 4};
+  const myObj2 = {myNum: 4};
+
+  expect(myObj).toStrictEqual(myObj2);
+})
+
+test('toEqual', () => {
+  const myObj = {myNum: 4};
+  const myObj2 = {myNum: 4};
+
+  expect(myObj).toEqual(myObj2);
+})
